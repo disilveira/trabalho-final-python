83b9f73098afae20a6afa4afc45b2d1a19c950a3
FranciscoGB <franciscogrynberg34@gmail.com>
2021-06-24 14:50:09 -0300

    criação de métodos para exibição de contas a pagar por mês no fluxo de caixa

--- app/models/contasPagar.py
+++ app/models/contasPagar.py
@@ -1,5 +1,7 @@
+from django.db.models.aggregates import Count
 from . import Classificacao, formaPagamento
 from django.db import models
+from django.db.models import Sum
 
 class contasPagarManager(models.Manager):
     def get_contasPagar(self):
@@ -7,6 +9,18 @@ class contasPagarManager(models.Manager):
 
     def gerar_relatorio(self):
         return contasPagar.objects.order_by('dataVencimento')
+
+    def obter_somaPrevisto_mes(self, i):
+        soma = float(contasPagar.objects.filter(dataVencimento__month=i, situacao=False).aggregate(Sum('valor'))['valor__sum'] or 0)
+        return soma
+
+    def obter_somaRealizado_mes(self, i):
+        soma = float(contasPagar.objects.filter(dataVencimento__month=i, situacao=True).aggregate(Sum('valor'))['valor__sum'] or 0)
+        return soma
+
+    def obter_soma_mes(self, i):
+        soma = float(contasPagar.objects.filter(dataVencimento__month=i).aggregate(Sum('valor'))['valor__sum'] or 0)
+        return soma
 
 
 class contasPagar(models.Model):
--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from .serializers import classificacaoSerializer, contasPagarSerializer, contasR
 from .models import Classificacao, formaPagamento, contasPagar, contasReceber
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
-from django.views.decorators.csrf import csrf_exempt
+import datetime
 
 class classificacaoViewSet(viewsets.ModelViewSet):
     queryset = Classificacao.objects.all()
@@ -32,5 +32,17 @@ def exibir_relatorios(request):
 @require_http_methods(['GET'])
 def exibir_fluxo(request):
 
+    meses = []
 
-    return render(request, 'fluxo.html')
+    for i in range(1, 6):
+        mes = {
+            "data" : datetime.date(1900, i, 1).strftime('%B'),
+            "pagar" : {
+                "somaPrevisto": contasPagar.objects.obter_somaPrevisto_mes(i),
+                "somaRealizado": contasPagar.objects.obter_somaRealizado_mes(i),
+                "somaMensal": contasPagar.objects.obter_soma_mes(i),
+            }
+        } 
+        meses.append(mes)
+
+    return render(request, 'fluxo.html', {"meses" : meses})
